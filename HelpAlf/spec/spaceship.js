c7b7a6778051dff4ffb7c43e709586fdebb58433
GiGaFHTW <if25b075@technikum-wien.at>
2026-06-09 06:40:52 +0200

    failing test 3
    + some syntax/logic fixes

--- HelpAlf/spec/spaceship.js
+++ HelpAlf/spec/spaceship.js
@@ -3,14 +3,19 @@ describe('findSpaceship', function() {
 		spaceship = new Spaceship();
 
 		emptyMap = '';
-		mapWithNoX = '.....\n.....\n.....\n.....'
+		mapWithNoX = '.....\n.....\n.....\n.....\n.....'
+		mapWithX = '.....\n.....\n.....\n..X..\n.....'
 	});
 
-	it('empty map', function() {
+	it('empty map should return "Spaceship lost forever."', function() {
 		expect(spaceship.find(emptyMap)).toEqual('Spaceship lost forever.');
 	});
 
-	it('map with no X', function() {
+	it('map with no X should return "Spaceship lost forever."', function() {
 		expect(spaceship.find(mapWithNoX)).toEqual('Spaceship lost forever.');
+	});
+
+	it('map X should return Coordinates of Spaceship [x = 2, y = 1]', function() {
+		expect(spaceship.find(mapWithX)).toEqual([2, 1]);
 	});
 });

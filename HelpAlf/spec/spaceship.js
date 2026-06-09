f3bf86ba6b1347c94057289d5b8412a54f07e4e3
GiGaFHTW <if25b075@technikum-wien.at>
2026-06-09 06:34:43 +0200

    test 2, already passed, no productive code, no refactoring

--- HelpAlf/spec/spaceship.js
+++ HelpAlf/spec/spaceship.js
@@ -3,10 +3,14 @@ describe('findSpaceship', function() {
 		spaceship = new Spaceship();
 
 		emptyMap = '';
+		mapWithNoX = '.....\n.....\n.....\n.....'
 	});
 
 	it('empty map', function() {
 		expect(spaceship.find(emptyMap)).toEqual('Spaceship lost forever.');
 	});
 
+	it('map with no X', function() {
+		expect(spaceship.find(mapWithNoX)).toEqual('Spaceship lost forever.');
+	});
 });

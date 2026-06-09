43e5a6699f76d2a1e0527f8a7d7fd1706550abd9
GiGaFHTW <if25b075@technikum-wien.at>
2026-06-09 06:50:38 +0200

    productive code 3, no refactor needed

--- HelpAlf/src/spaceship.js
+++ HelpAlf/src/spaceship.js
@@ -2,5 +2,18 @@
 function Spaceship() { }
 
 Spaceship.prototype.find = function(map) {
-	return "Spaceship lost forever."
+	
+	var rows = map.split('\n');
+	rows.reverse();
+
+	for(let y = 0; y < rows.length; y++) {
+		for(let x = 0; x < rows[y].length; x++) {
+			if(rows[y][x] == 'X') {
+				return [x, y]
+			}
+		}
+	}
+	
+	
+	return "Spaceship lost forever.";
 }
